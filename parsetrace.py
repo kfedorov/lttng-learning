90a88a9207744fa9d285ff54a9e5f169c670f421
Konstantin Fedorov <fedorov.konstantin@gmail.com>
2017-03-15 11:22:27 -0400

    No progress

--- parsetrace.py
+++ parsetrace.py
@@ -5,74 +5,109 @@ import numpy as np
 import pickle
 import os.path
 
-save_dir = 'pickle/'
 
-def parse(trace_file, window_size=250):
-    if not os.path.exists(save_dir):
-        os.makedirs(save_dir)
+class traceParser:
+    windows = []
+    i_to_name = {}
+    name_to_i = {}
+    i_to_comm = {}
+    comm_to_i = {}
 
-    if os.path.isfile(save_dir+'windows.p'):
-        return pickle.load(open(save_dir+'windows.p', 'rb'))
-
-    # A trace collection contains one or more traces
-    col = babeltrace.TraceCollection()
-
-    # Add the trace provided by the user (LTTng traces always have
-    # the 'ctf' format)
-    if col.add_trace(trace_file, 'ctf') is None:
-        raise RuntimeError('Cannot add trace')
-
-    names = set()
-    commands = set()
-    event_count = 0
-    for event in col.events:
-        event_count += 1
-        names.add(event.name)
-        if event.name == 'sched_switch':
-            commands.add(event['prev_comm'])
-
-    i_to_name = dict(enumerate(names))
-    name_to_i = {v: k for k, v in i_to_name.items()}
-
-    i_to_comm = dict(enumerate(commands))
-    comm_to_i = {v: k for k, v in i_to_comm.items()}
-
-    pickle.dump(i_to_name, open(save_dir+'i2n.p', 'wb'))
-    pickle.dump(i_to_comm, open(save_dir+'i2c.p', 'wb'))
+    def __init__(self, trace_file, trace_name, window_size=250):
+        self.trace_file = trace_file
+        self.trace_name = trace_name
+        self.window_size = window_size
+        self.save_dir = 'pickle/' + trace_name + '/'
+        self.parse()
 
 
-    cur_comms = {}
+    def parse(self):
+        if not os.path.exists(self.save_dir):
+            os.makedirs(self.save_dir)
 
-    window_count = np.floor(event_count / window_size).astype(int)
-    windows = np.zeros([window_count, len(commands), len(names)], int)
-    cur_window = 0
-    window_event_count = 0
-    # Iterate on events
-    for event in col.events:
-        window_event_count += 1
-        if window_event_count > window_size:
-            window_event_count = 1
-            cur_window += 1
-            if cur_window >= window_count:
-                break
+        if os.path.isfile(self.save_dir+'windows.p'):
+            self.windows = pickle.load(open(self.save_dir+'windows.p', 'rb'))
+            self.i_to_name = pickle.load(open(self.save_dir+'i2n.p', 'rb'))
+            self.i_to_comm = pickle.load(open(self.save_dir+'i2c.p', 'rb'))
+            self.name_to_i = {v: k for k, v in self.i_to_name.items()}
+            self.comm_to_i = {v: k for k, v in self.i_to_comm.items()}
+            return self.windows
 
-        cur_cpu = event['cpu_id']
-        # if switch, update current task on cpu
-        if event.name == 'sched_switch':
-            cur_comms[cur_cpu] = event['next_comm']
-            continue
+        # A trace collection contains one or more traces
+        col = babeltrace.TraceCollection()
 
-        comm = cur_comms[cur_cpu]
-        # if we don't know the current command, continue
-        if comm is None:
-            continue
+        # Add the trace provided by the user (LTTng traces always have
+        # the 'ctf' format)
+        if col.add_trace(self.trace_file, 'ctf') is None:
+            raise RuntimeError('Cannot add trace')
 
-        windows[cur_window][comm_to_i.get(comm)][name_to_i.get(event.name)] += 1
+        names = set()
+        commands = set()
+        event_count = 0
+        for event in col.events:
+            event_count += 1
+            names.add(event.name)
+            if event.name == 'sched_switch':
+                commands.add(event['prev_comm'])
 
-    windows = windows.reshape([window_count, len(commands) * len(names)])
+        self.i_to_name = dict(enumerate(names))
+        self.name_to_i = {v: k for k, v in self.i_to_name.items()}
 
-    pickle.dump(windows, open(save_dir+'windows.p', 'wb'))
-    return windows
+        self.i_to_comm = dict(enumerate(commands))
+        self.comm_to_i = {v: k for k, v in self.i_to_comm.items()}
+
+        pickle.dump(self.i_to_name, open(self.save_dir+'i2n.p', 'wb'))
+        pickle.dump(self.i_to_comm, open(self.save_dir+'i2c.p', 'wb'))
+
+        cur_comms = {}
+
+        window_count = np.floor(event_count / self.window_size).astype(int)
+        windows = np.zeros([window_count, len(commands), len(names)], int)
+        cur_window = 0
+        window_event_count = 0
+        # Iterate on events
+        for event in col.events:
+            window_event_count += 1
+            if window_event_count > self.window_size:
+                window_event_count = 1
+                cur_window += 1
+                if cur_window >= window_count:
+                    break
+
+            cur_cpu = event['cpu_id']
+            # if switch, update current task on cpu
+            if event.name == 'sched_switch':
+                cur_comms[cur_cpu] = event['next_comm']
+                continue
+
+            comm = cur_comms[cur_cpu]
+            # if we don't know the current command, continue
+            if comm is None:
+                continue
+
+            windows[cur_window][self.comm_to_i.get(comm)][self.name_to_i.get(event.name)] += 1
+
+        windows = np.divide(windows, self.window_size)
+        self.windows = windows.reshape([window_count, len(commands) * len(names)])
+
+        pickle.dump(self.windows, open(self.save_dir+'windows.p', 'wb'))
+        return self.windows
+
+    def windowToString(self, window, separator='\n'):
+        description = ''
+        window = np.multiply(window, self.window_size).astype(int)
+        window_shaped = window.reshape([len(self.comm_to_i), len(self.name_to_i)])
+        for i,names in enumerate(window_shaped):
+            used_names = names > 0
+            if any(used_names):
+                description += str(self.i_to_comm[i]) + separator
+                used_names_idx = np.where(used_names)
+                for name_idx  in used_names_idx[0]:
+                    description += str(self.i_to_name[name_idx]) + ": " + str(names[name_idx]) + '; '
+                description += separator
+        return description
+
 
 if __name__ == '__main__':
-    sys.exit(0 if parse(trace_file=sys.argv[1]).any() else 1)
+    traceP = traceParser('/home/kfedorov/lttng-traces/firefox/kernel/','firefox')
+    print(traceP.windowToString(traceP.windows[254], separator='|'))
